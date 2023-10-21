cb8f8cca85f83d50754c487c70f675059c3b232b
Md. Abir mahmud <abirmahmud5665@gmail.com>
2023-10-21 22:20:30 +0600

    fix on db rui

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const cors = require("cors");
 app.use(cors(corsConfig));
 app.use(express.json());
 
-const uri = `mongodb+srv://abirmahmud5665:KzQIHdW6yh6gMwel@cluster0.ghspuwt.mongodb.net/npinternational`;
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dgljxbc.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dgljxbc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
